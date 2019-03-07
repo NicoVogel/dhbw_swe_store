3b1f9e35645e2f3225e0e80262b9e5fdac836723
BirdIT <nico-vogel97@outlook.de>
2019-03-08 00:34:24 +0100

    bugfix

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,19 +2,19 @@ drop table if exists customer;
 drop table if exists product;
 
 create table customer(
-	id serial primary key,
+	id serial,
 	name text not null check(name <> ''),
     address text not null check(address <> ''),
     primary key (id)
 );
 
 create table product(
-	id serial primary key,
+	id serial,
 	count int, 
     description text,
     categorie text,
     sellPrice numeric(6,2),
     buyPrice numeric(6,2),
-    text supplyer,
+    supplyer text,
     primary key(id)
 );
