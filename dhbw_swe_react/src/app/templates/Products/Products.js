7f4dc6b3ce9ebd9ac3ddc77b65890539ff8710fe
Leopold Stenger <leopold.stenger@uid.com>
2019-03-23 10:41:59 +0100

    import table component to templates

--- dhbw_swe_react/src/app/templates/Products/Products.js
+++ dhbw_swe_react/src/app/templates/Products/Products.js
@@ -1,14 +1,242 @@
 import React, { Component } from 'react';
-import { faDivide } from '@fortawesome/free-solid-svg-icons';
+import './Products.scss';
 
 import Headline from '../../atoms/Headline/Headline';
+import Table from '../../organisms/Table/Table';
+
+
 class Products extends Component {
     state = {  }
     render() { 
+        const productList = [
+            {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            },
+            {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            },
+            {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            },
+            {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            },
+            {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            },
+            {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            },
+            {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            },
+            {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            },
+            {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            },
+            {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            },
+            {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            },
+            {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            },
+            {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            },
+            {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            },
+            {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            },
+            {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            },
+            {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            },
+            {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            }, {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            },
+            {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            },
+            {
+                count: 13,
+                description: 'Kartoffel',
+                category: 'Gemüse',
+                sellPrice: 1.29,
+                buyPrice: 0.89,
+                supplier: 'Müller gmbH',
+                origin: 'Deutschland'
+            },
+        ];
+
+        const productTableHeaders = [
+            {
+                id: 'count',
+                text: 'Anzahl im Lager'
+            },
+            {
+                id: 'description',
+                text: 'Produkt Bezeichung'
+            },
+            {
+                id: 'category',
+                text: 'Kategorie'
+            },
+            {
+                id: 'sellPrice',
+                text: 'Verkaufspreis in €'
+            },
+            {
+                id: 'buyPrice',
+                text: 'EInkaufspreis in €'
+            },
+            {
+                id: 'supplier',
+                text: 'Hersteller'
+            },
+            {
+                id: 'origin',
+                text: 'Herkunft'
+            }
+        ]
         return ( 
-            <div className="products">
-                <Headline text='Produktübersicht'/>
+            <div className="products-container">
+                <Headline text='Produktübersicht' />
+                <div className="table-container">
+                    <Table tableHeaders={productTableHeaders} tableData={productList}/>
+                </div>
             </div>
+            
         );
     }
 }
--- dhbw_swe_react/src/app/templates/Suppliers/Suppliers.js
+++ dhbw_swe_react/src/app/templates/Suppliers/Suppliers.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 
 import Headline from '../../atoms/Headline/Headline';
+import Table from '../../organisms/Table/Table';
+import { faDivide } from '@fortawesome/free-solid-svg-icons';
 
 class Suppliers extends Component {
     state = {  }
     render() { 
+
+        const suppliersTableHeaders = [
+            {
+                text: 'Name'
+            },
+            {
+                text: 'Adresse'
+            }
+        ];
+
+        const suppliersList = [
+            {
+                name: 'Müller GmbH',
+                addresse: '012546 Stuttgart Wiesenstraße 128'
+            }
+        ];
+
         return ( 
-            <Headline text='Lieferanten' />
+            <div>
+            <Headline text='Lieferanten'/>
+            <Table tableHeaders={suppliersTableHeaders} tableData={suppliersList} />
+            </div>
         );
     }
 }
