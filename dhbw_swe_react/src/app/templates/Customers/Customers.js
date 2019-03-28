966ffa889b3b110f43304463cc9736845266e745
Viet Do <vietdonguyen@hotmail.de>
2019-03-28 22:27:34 +0100

    add customer table

--- dhbw_swe_react/src/app/templates/Customers/Customers.js
+++ dhbw_swe_react/src/app/templates/Customers/Customers.js
@@ -1,15 +1,58 @@
 import React, { Component } from 'react';
-
+import './Customer.scss';
 
 import Headline from '../../atoms/Headline/Headline';
+import Table from '../../organisms/Table/Table';
 
 class Customers extends Component {
-    state = {  }
-    render() { 
-        return (
-            <Headline text='Kunden' />
-        );
-    }
+  constructor() {
+    super();
+    this.state = {
+      customerList: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://viet.nat.selfnet.de:32405/kunde')
+      .then(results => results.json())
+      .then((data) => {
+        this.setState({
+          customerList: data._embedded.kunde,
+        });
+      });
+  }
+
+  render() {
+    const productTableHeaders = [
+      {
+        id: 'name',
+        text: 'Name',
+      },
+      {
+        id: 'address',
+        text: 'Adresse',
+      },
+
+    ];
+    const {
+      customerList,
+    } = this.state;
+    return (
+      <div className="customer-container">
+        <Headline text="Kundenübersicht" />
+        <div className="table-container">
+          <Table
+            tableHeaders={
+            productTableHeaders
+          }
+            tableData={
+            customerList
+          }
+          />
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Customers;
