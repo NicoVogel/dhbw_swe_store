e9e257eb2c55f00188c19e4d1dc68841bf32b769
Nico Vogel <nico-vogel97@outlook.de>
2019-04-02 23:42:54 +0200

    add shell build script

---
+++ buildAndRun.sh
@@ -0,0 +1,20 @@
+#!/bin/besh
+
+MOUNT_PATH = $(pwd)
+USER_PATH = Path-For-Docker -Path $env:USERPROFILE
+#https://hub.docker.com/_/maven#reusing-the-maven-local-repository
+VOLUME_M2_PATH="~/.m2:/root/.m2"
+#https://hub.docker.com/_/maven#how-to-use-this-image
+VOLUME_PROJECT="${MOUNT_PATH}/dhbw_swe_server:/usr/src/mymaven"
+VOLUME_NODE="${MOUNT_PATH}/dhbw_swe_react:/home/node"
+
+echo "build java server"
+docker run -it -v $VOLUME_M2_PATH -v $VOLUME_PROJECT -w /usr/src/mymaven maven:alpine mvn install -e dockerfile:build
+
+echo "build react application"
+echo "node code volume: ${VOLUME_NODE}"
+# why --no-bin-links is required https://forums.docker.com/t/symlinks-on-shared-volumes-not-supported/9288/3
+docker run -it -e NODE_ENV=production -v $VOLUME_NODE node:alpine /bin/sh -c "cd /home/node && npm install && npm run build"
+
+echo "start server and database"
+docker-compose up
