a99c1f3bfcb12f9d399b16ee45f1dae4c912f351
Viet Do <vietdonguyen@hotmail.de>
2019-04-04 17:54:06 +0200

    remove supplier table

--- dhbw_swe_react/src/app/templates/Suppliers/Suppliers.js
+++ dhbw_swe_react/src/app/templates/Suppliers/Suppliers.js
@@ -5,19 +5,10 @@ import Table from '../../organisms/Table/Table';
 
 class Suppliers extends Component {
   render() {
-    const defaultTableHeaders = ['name', 'address'];
-
-    const suppliersList = [
-      {
-        name: 'Müller GmbH',
-        address: '012546 Stuttgart Wiesenstraße 128',
-      },
-    ];
 
     return (
       <div>
         <Headline text="Lieferanten" />
-        <Table defaultTableHeaders={defaultTableHeaders} tableData={suppliersList} />
       </div>
     );
   }
