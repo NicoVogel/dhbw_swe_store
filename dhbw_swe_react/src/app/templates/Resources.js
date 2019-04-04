6188392274ce7e6943c07b81f36424fb72590abf
Viet Do <vietdonguyen@hotmail.de>
2019-04-04 14:08:24 +0200

    change quote

--- dhbw_swe_react/src/app/templates/Resources.js
+++ dhbw_swe_react/src/app/templates/Resources.js
@@ -1,6 +1,6 @@
-export const SERVER_ADDRESS = "http://viet.nat.selfnet.de:32404";
-export const REST_CUSTOMER = "/api/kunde";
-export const REST_PRODUCT = "/api/produkt";
+export const SERVER_ADDRESS = 'http://viet.nat.selfnet.de:32404';
+export const REST_CUSTOMER = '/api/kunde';
+export const REST_PRODUCT = '/api/produkt';
 
 const headerStrings = new Map();
 
