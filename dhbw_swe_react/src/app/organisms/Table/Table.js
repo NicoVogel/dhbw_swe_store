3f6bb4f93945e81f5b24dbf51f6b22a3a339b04c
Viet Do <vietdonguyen@hotmail.de>
2019-04-04 14:40:18 +0200

    rewrite table value output

--- dhbw_swe_react/src/app/organisms/Table/Table.js
+++ dhbw_swe_react/src/app/organisms/Table/Table.js
@@ -38,10 +38,8 @@ class Table extends Component {
                   <td id="hiddencolumn" />
                   {
                     // TODO change defaultValue to value nad set change handler
-                    Object.keys(dataObject).map((key) => {
-                      if (key !== '_links') {
-                        return <td><input className="input-field" type="text" defaultValue={dataObject[key]} /></td>;
-                      } return null;
+                    Object.keys(dataObject).filter(key => key !== '_links').map((key) => {
+                      return <td><input className="input-field" type="text" key={`${key}`} defaultValue={dataObject[key]} /></td>;
                     })
                   }
                 </tr>
