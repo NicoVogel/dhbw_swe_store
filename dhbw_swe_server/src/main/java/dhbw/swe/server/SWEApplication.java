3f6c15c099e16829e55dcf6568a8fb5109dd2ea7
Nico Vogel <nico-vogel97@outlook.de>
2019-04-04 16:39:20 +0200

    move volume

--- dhbw_swe_server/src/main/java/dhbw/swe/server/SWEApplication.java
+++ dhbw_swe_server/src/main/java/dhbw/swe/server/SWEApplication.java
@@ -1,13 +1,31 @@
 package dhbw.swe.server;
 
 
+import java.util.Arrays;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class SWEApplication {
 
+	@Autowired
+	Environment env;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SWEApplication.class, args);
+	}
+	
+	@PostConstruct
+	public void info() {
+		log.info(Arrays.toString(env.getActiveProfiles()));
+		log.info(Arrays.toString(env.getDefaultProfiles()));
 	}
 }
