a9e8d30631dac6a66fef5fbd6bc2427c024fbb83
Nico Vogel <nico-vogel97@outlook.de>
2019-04-05 08:29:05 +0200

    add request mapping for react

--- dhbw_swe_server/src/main/java/dhbw/swe/server/SWEApplication.java
+++ dhbw_swe_server/src/main/java/dhbw/swe/server/SWEApplication.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -28,4 +29,9 @@ public class SWEApplication {
 		log.info(Arrays.toString(env.getActiveProfiles()));
 		log.info(Arrays.toString(env.getDefaultProfiles()));
 	}
+	
+	@RequestMapping(value = {"/","/products","/producers","/suppliers", "/customers", "/deliverynote", "/search", "/settings"})
+    public String index() {
+        return "index";
+    }
 }
