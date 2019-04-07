79dcbb5e5132378309d26b21fc1d100a6d477274
Viet Do <vietdonguyen@hotmail.de>
2019-04-07 23:01:13 +0200

    add content to detailview

--- dhbw_swe_react/src/app/organisms/DetailView/DetailView.js
+++ dhbw_swe_react/src/app/organisms/DetailView/DetailView.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import './DetailView.scss';
 import Headline from '../../atoms/Headline/Headline';
+import { headerStrings } from '../../templates/Resources';
+
 
 const DetailView = ({ element }) => {
   const { category, description } = element;
   return (
-    <div className="detailview">
+    <div className="detailview-container">
       <Headline text={`${category}: ${description}`} />
-      {JSON.stringify(element)}
+      <div className="list-body">
+        {
+          Object.keys(element).filter(key => key !== '_links').map(key => (
+            <div className="list-row" key={`${key}`}>
+              <div className="key">
+                {headerStrings.get(key)}
+              </div>
+              <div className="value">
+                {element[key]}
+              </div>
+            </div>
+          ))
+        }
+      </div>
+
     </div>
   );
 };
