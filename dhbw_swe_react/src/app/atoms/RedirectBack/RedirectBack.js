c4a27f7ef8fd2f1f18fd24d0a2e4a09dfd463238
Viet Do <vietdonguyen@hotmail.de>
2019-04-07 23:37:15 +0200

    css for the win ^^

--- dhbw_swe_react/src/app/atoms/RedirectBack/RedirectBack.js
+++ dhbw_swe_react/src/app/atoms/RedirectBack/RedirectBack.js
@@ -1,11 +1,19 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faArrowLeft,
+} from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import './RedirectBack.scss';
+
+library.add(faArrowLeft);
 
 const RedirectBack = (props) => {
   const { history, text } = props;
   return (
     <div className="redirect-back">
       <button onClick={history.goBack} type="submit">
+        <FontAwesomeIcon icon="arrow-left" className="icon" />
         { text }
       </button>
     </div>
