8cea97586e0ee27903ca92add59d098958916ffb
Viet Do <vietdonguyen@hotmail.de>
2019-04-07 19:28:18 +0200

    add RedirectBack atom

---
+++ dhbw_swe_react/src/app/atoms/RedirectBack/RedirectBack.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './RedirectBack.scss';
+
+const RedirectBack = text => (
+  <div className="redirect-back">
+    {text}
+  </div>
+);
+
+
+export default RedirectBack;
