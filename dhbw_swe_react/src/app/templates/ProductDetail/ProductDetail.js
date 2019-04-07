2e4f3d4cb742fe0339a63e8297c030d05be260a6
Viet Do <vietdonguyen@hotmail.de>
2019-04-07 21:44:34 +0200

    add fetching of individual element

--- dhbw_swe_react/src/app/templates/ProductDetail/ProductDetail.js
+++ dhbw_swe_react/src/app/templates/ProductDetail/ProductDetail.js
@@ -4,15 +4,31 @@ import Headline from '../../atoms/Headline/Headline';
 import RedirectBack from '../../atoms/RedirectBack/RedirectBack';
 import { SERVER_ADDRESS, REST_LINKS } from '../Resources';
 
+const axios = require('axios');
 
 const ProductDetail = (props) => {
   const { history, match } = props;
 
+  const getProductInfo = () => {
+    // let test;
+    axios.get(`${SERVER_ADDRESS}${REST_LINKS.get('product')}/${match.params.productID}`)
+      .then((results) => {
+        if (results.status === 200) {
+          console.log(results.data);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    // return test;
+  };
+
+  getProductInfo();
+
   return (
     <div className="productDetail-container">
       <Headline text="CATEGORY: PRODUCT" />
-      <RedirectBack history={history} text="Zurück zur Produktübersicht" />
-      {`${SERVER_ADDRESS}${REST_LINKS.get('product')}/${match.params.productID}`}
+      <RedirectBack history={history} text="Zurück zur Produktübersicht" /> 
     </div>
   );
 };
