bfa770e863342a9bb38a8eb5c625ff6c56541320
Viet Do <vietdonguyen@hotmail.de>
2019-04-08 13:03:17 +0200

    fix master branch haha

---
+++ dhbw_swe_react/src/app/templates/Settings/Settings.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Headline from '../../atoms/Headline/Headline';
+
+class Settings extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <Headline text='Einstellungen' />
+        );
+    }
+}
+ 
+export default Settings;
